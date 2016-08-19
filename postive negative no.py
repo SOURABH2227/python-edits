4cffb34212a4633615fabf057b403387d79199ed
SOURABH2227 <SOURABH2227@GMAIL.COM>
2016-08-19 22:32:47 +0530

    Add files via upload

---
+++ postive negative no.py
@@ -0,0 +1,7 @@
+#To check if the entered number is +ve or -ve & display an correct message
+
+num=float(input("Enter a number:"))
+if num >= 0:
+    print("positive or zero")
+else:
+    print("Negative number")
